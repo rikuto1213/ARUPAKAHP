741d7774d210b4a9d4265825023e3b232c47ca17
rikuto1213 <nisriku@gmail.com>
2025-12-17 17:23:55 +0900

    feat: Add Prisma for contact message management and a new Next.js API route.

--- prisma/migrations/20251217080626_init_postgres/migration.sql
+++ prisma/migrations/20251217080626_init_postgres/migration.sql
@@ -1,9 +1,11 @@
 -- CreateTable
 CREATE TABLE "ContactMessage" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "subject" TEXT NOT NULL,
     "message" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ContactMessage_pkey" PRIMARY KEY ("id")
 );
